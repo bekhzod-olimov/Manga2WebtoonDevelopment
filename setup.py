da8fde9f80e0deb8ab4aee7ce4efce5e4d0ce851
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2024-09-24 16:09:11 +0900

    setup script

--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python
 # Copyright (c) Facebook, Inc. and its affiliates.
 
-import glob
-import os
-import shutil
+# Import libraries
+import os, torch, shutil, glob
 from os import path
 from setuptools import find_packages, setup
 from typing import List
-import torch
 from torch.utils.cpp_extension import CUDA_HOME, CppExtension, CUDAExtension
 
 torch_ver = [int(x) for x in torch.__version__.split(".")[:2]]
 assert torch_ver >= [1, 8], "Requires PyTorch >= 1.8"
-
 
 def get_version():
     init_py_path = path.join(path.abspath(path.dirname(__file__)), "detectron2", "__init__.py")
